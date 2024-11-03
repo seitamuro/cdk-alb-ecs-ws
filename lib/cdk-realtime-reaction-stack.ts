bacd455c9df2ce6ba2beadb49560c1f6052f886f
seitamuro <seitamuro0210@gmail.com>
2024-11-03 19:07:46 +0900

    未認証ユーザー用のRoleを用意 + SNSトピックを用意

--- lib/cdk-realtime-reaction-stack.ts
+++ lib/cdk-realtime-reaction-stack.ts
@@ -1,16 +1,42 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib";
+import * as cognito from "aws-cdk-lib/aws-cognito";
+import * as iam from "aws-cdk-lib/aws-iam";
+import * as sns from "aws-cdk-lib/aws-sns";
+import { Construct } from "constructs";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkRealtimeReactionStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const reactionTopic = new sns.Topic(this, "ReactionTopic", {
+      displayName: "ReactionTopic",
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkRealtimeReactionQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const identityPool = new cognito.CfnIdentityPool(this, "IdentityPool", {
+      allowUnauthenticatedIdentities: true,
+    });
+
+    const unauthenticatedRole = new iam.Role(this, "unauthRole", {
+      assumedBy: new iam.FederatedPrincipal("cognito-identity.amazonaws.com", {
+        StringEquals: {
+          "cognito-identity.amazonaws.com:aud": identityPool.ref,
+        },
+        "ForAnyValue:StringLike": {
+          "cognito-identity.amazonaws.com:amr": "unauthenticated",
+        },
+      }),
+      inlinePolicies: {
+        policy: new iam.PolicyDocument({
+          statements: [
+            new iam.PolicyStatement({
+              actions: ["sns:Subscribe"],
+              effect: iam.Effect.ALLOW,
+              resources: [reactionTopic.topicArn],
+            }),
+          ],
+        }),
+      },
+    });
   }
 }
